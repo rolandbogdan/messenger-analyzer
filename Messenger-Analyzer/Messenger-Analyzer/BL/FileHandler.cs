104ce1ed09d3a8154591e85e055e4c4b0e4fccbb
Roland Bogdan <bogdanroland07@gmail.com>
2021-05-27 12:51:21 +0200

    Formatting of strings not working yet

--- Messenger-Analyzer/Messenger-Analyzer/BL/FileHandler.cs
+++ Messenger-Analyzer/Messenger-Analyzer/BL/FileHandler.cs
@@ -20,9 +20,11 @@ namespace Messenger_Analyzer.BL
 
         private void ProcessOne(string path)
         {
-            StreamReader sr = new StreamReader(path);
+            StreamReader sr = new StreamReader(path, Encoding.UTF8);
             string json = sr.ReadToEnd();
             sr.Close();
+
+            // this.FixString(ref json);
 
             Chat content = JsonConvert.DeserializeObject<Chat>(json);
             this.Chats.Add(content);
@@ -34,6 +36,31 @@ namespace Messenger_Analyzer.BL
             {
                 this.ProcessOne(item);
             }
+        }
+        
+        private void FixString(ref string x)
+        {
+            x.Replace(@"\u00c3\u00a1", "á");
+            x.Replace(@"\u00c3\u00a9", "é");
+            x.Replace(@"\u00c3\u00ad", "í");
+            x.Replace(@"\u00c3\u00b3", "ó");
+            x.Replace(@"\u00c3\u00b6", "ö");
+            x.Replace(@"\u00c5\u0091", "ő");
+            x.Replace(@"\u00c3\u00ba", "ú");
+            x.Replace(@"\u00c3\u00bc", "ü");
+            x.Replace(@"\u00c5\u00b1", "ű");
+
+            x.Replace(@"\u00c3\u0081", "Á");
+            x.Replace(@"\u00c3\u0089", "É");
+            x.Replace(@"\u00c3\u008d", "Í");
+            x.Replace(@"\u00c3\u0093", "Ó");
+            x.Replace(@"\u00c3\u0096", "Ö");
+            x.Replace(@"\u00c5\u0090", "Ő");
+            x.Replace(@"\u00c3\u009a", "Ú");
+            x.Replace(@"\u00c3\u009c", "Ü");
+            x.Replace(@"\u00c5\u00b0", "Ű");
+
+            x.Replace(@"\u00c3\u00a4", "ä");
         }
     }
 }
